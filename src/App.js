5a6df58ce175e3023dd0daacaff2764638feafff
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2022-11-16 01:51:17 +0600

    add signup page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.min';
+import SignUp from './Pages/Login/Signup/SignUp';
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Header/>
+     <Routes>
+      <Route path='/signup' element={<SignUp/>} />
+     </Routes>
     </div>
   );
 }
---
+++ src/Pages/Login/Signup/SignUp.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './SignUp.css';
+
+const SignUp = () => {
+    return (
+        <div className='container'>
+            <div className='signup'>
+                <div className='signup-dev'>
+
+                    <h3 className='fw-bold text-center mt-5'>Sign Up</h3>
+
+                    <form className='register-form'>
+                        <div className='register-input'>
+                            <input className='mx-auto' type='text' placeholder='Enter name' required /> <br />
+                            <input type='email' placeholder='Enter email' required /> <br />
+                            <input type='password' placeholder='Enter password' required /> <br />
+                            <input className='fw-bold' type='submit' value='Sign Up' />
+                        </div>
+                    </form>
+
+                    <div className='d-flex justify-content-center align-items-center mt-3'>
+                        <hr style={{ width: '40%', margin: 'auto',  }} />
+                        <p>OR</p>
+                        <hr style={{ width: '40%', margin: 'auto',  }} />
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default SignUp;
